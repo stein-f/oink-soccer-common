864891d38fb92e864490de42dd8170777411de93
stein-f <hi@stein.wtf>
2023-11-18 22:43:53 +0000

    scaling function tweaks

--- scaling.go
+++ scaling.go
@@ -1,5 +1,7 @@
 package soccer
 
+const scalingConstant = float64(1.3368054448513546e-06)
+
 // ScalingFunction is a function that aims to give higher rated players a more significant advantage over lower rated players.
 // If we took the raw player ratings, then a lower skilled player on 80 would have almost the same ability as a higher skilled player 84, given the random nature of the game.
 // This function aims to give higher rated players a more significant advantage over lower rated players.
@@ -7,6 +9,5 @@ package soccer
 // y = ax^b
 // where y is the scaled rating, x is the original rating (0-100) and a and b are constants that can be adjusted to change the shape of the curve.
 func ScalingFunction(originalRating float64) float64 {
-	const a float64 = 0.0001
-	return a * originalRating * originalRating * originalRating
+	return scalingConstant * originalRating * originalRating * originalRating * originalRating
 }
