68dbb76c7a2980d82819b7d7aa46772cd9410851
stein-f <hi@stein.wtf>
2023-11-20 11:38:28 +0000

    update simulation to make it easier to run

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -1,165 +1,27 @@
 package main
 
 import (
+	_ "embed"
+	"encoding/json"
 	"fmt"
 	soccer "github.com/stein-f/oink-soccer-common"
+	"log"
 )
 
-var homeLineup = soccer.GameLineup{
-	Team: soccer.Team{
-		ID:         "1",
-		CustomName: "StrongTeam",
-		Formation:  soccer.FormationTypeDiamond,
-	},
-	Players: []soccer.SelectedPlayer{
-		{
-			ID:   "1",
-			Name: "1",
-			Attributes: soccer.PlayerAttributes{
-				GoalkeeperRating: 90,
-				SpeedRating:      74,
-				DefenseRating:    33,
-				ControlRating:    21,
-				AttackRating:     37,
-				Position:         soccer.PlayerPositionGoalkeeper,
-			},
-			SelectedPosition: soccer.PlayerPositionGoalkeeper,
-		},
-		{
-			ID:   "2",
-			Name: "2",
-			Attributes: soccer.PlayerAttributes{
-				GoalkeeperRating: 14,
-				SpeedRating:      80,
-				DefenseRating:    90,
-				ControlRating:    81,
-				AttackRating:     37,
-				Position:         soccer.PlayerPositionDefense,
-			},
-			SelectedPosition: soccer.PlayerPositionDefense,
-		},
-		{
-			ID:   "3",
-			Name: "3",
-			Attributes: soccer.PlayerAttributes{
-				GoalkeeperRating: 14,
-				SpeedRating:      80,
-				DefenseRating:    55,
-				ControlRating:    85,
-				AttackRating:     91,
-				Position:         soccer.PlayerPositionMidfield,
-			},
-			SelectedPosition: soccer.PlayerPositionMidfield,
-		},
-		{
-			ID:   "4",
-			Name: "4",
-			Attributes: soccer.PlayerAttributes{
-				GoalkeeperRating: 11,
-				SpeedRating:      81,
-				DefenseRating:    75,
-				ControlRating:    88,
-				AttackRating:     71,
-				Position:         soccer.PlayerPositionMidfield,
-			},
-			SelectedPosition: soccer.PlayerPositionMidfield,
-		},
-		{
-			ID:   "5",
-			Name: "5",
-			Attributes: soccer.PlayerAttributes{
-				GoalkeeperRating: 14,
-				SpeedRating:      80,
-				DefenseRating:    22,
-				ControlRating:    85,
-				AttackRating:     93,
-				Position:         soccer.PlayerPositionAttack,
-			},
-			SelectedPosition: soccer.PlayerPositionAttack,
-		},
-	},
-}
+//go:embed home_team.json
+var homeTeamConfig []byte
 
-var awayLineup = soccer.GameLineup{
-	Team: soccer.Team{
-		ID:         "2",
-		CustomName: "WeakTeam",
-		Formation:  soccer.FormationTypeDiamond,
-	},
-	Players: []soccer.SelectedPlayer{
-		{
-			ID:   "6",
-			Name: "6",
-			Attributes: soccer.PlayerAttributes{
-				GoalkeeperRating: 65,
-				SpeedRating:      55,
-				DefenseRating:    12,
-				ControlRating:    33,
-				AttackRating:     2,
-				Position:         soccer.PlayerPositionGoalkeeper,
-			},
-			SelectedPosition: soccer.PlayerPositionGoalkeeper,
-		},
-		{
-			ID:   "7",
-			Name: "7",
-			Attributes: soccer.PlayerAttributes{
-				GoalkeeperRating: 14,
-				SpeedRating:      56,
-				DefenseRating:    68,
-				ControlRating:    81,
-				AttackRating:     11,
-				Position:         soccer.PlayerPositionDefense,
-			},
-			SelectedPosition: soccer.PlayerPositionDefense,
-		},
-		{
-			ID:   "8",
-			Name: "8",
-			Attributes: soccer.PlayerAttributes{
-				GoalkeeperRating: 14,
-				SpeedRating:      68,
-				DefenseRating:    65,
-				ControlRating:    76,
-				AttackRating:     72,
-				Position:         soccer.PlayerPositionMidfield,
-			},
-			SelectedPosition: soccer.PlayerPositionMidfield,
-		},
-		{
-			ID:   "9",
-			Name: "9",
-			Attributes: soccer.PlayerAttributes{
-				GoalkeeperRating: 11,
-				SpeedRating:      71,
-				DefenseRating:    67,
-				ControlRating:    71,
-				AttackRating:     55,
-				Position:         soccer.PlayerPositionMidfield,
-			},
-			SelectedPosition: soccer.PlayerPositionMidfield,
-		},
-		{
-			ID:   "10",
-			Name: "10",
-			Attributes: soccer.PlayerAttributes{
-				GoalkeeperRating: 14,
-				SpeedRating:      68,
-				DefenseRating:    22,
-				ControlRating:    67,
-				AttackRating:     70,
-				Position:         soccer.PlayerPositionAttack,
-			},
-			SelectedPosition: soccer.PlayerPositionAttack,
-		},
-	},
-}
+//go:embed away_team.json
+var awayTeamConfig []byte
 
 func main() {
 	var homeWins, awayWins, draws, goals int
 	gameCount := 500
 
 	scorerByPosition := make(map[soccer.PlayerPosition]int)
+
+	homeLineup := loadConfig(homeTeamConfig)
+	awayLineup := loadConfig(awayTeamConfig)
 
 	for i := 0; i < gameCount; i++ {
 		gameEvents, err := soccer.RunGame(homeLineup, awayLineup)
@@ -209,4 +71,12 @@ func main() {
 	fmt.Printf("Draws: %d\n", draws)
 	fmt.Printf("Goals/game: %f\n", goalsPerGame)
 	fmt.Printf("Scorer by position: %v\n", scorerByPosition)
+}
+
+func loadConfig(config []byte) soccer.GameLineup {
+	var lineup soccer.GameLineup
+	if err := json.Unmarshal(config, &lineup); err != nil {
+		log.Fatal(err)
+	}
+	return lineup
 }
