58e94f9249fd8de6dedabc2193e237bc1805c247
stein <hi@stein.wtf>
2024-08-21 11:43:23 +0100

    bump injury frequency

--- injuries.go
+++ injuries.go
@@ -12,12 +12,12 @@ func GetAllInjuries() []Injury {
 }
 
 var injuryWeightsDefaults = []weightedrand.Choice{
-	{Item: false, Weight: 20},
+	{Item: false, Weight: 15},
 	{Item: true, Weight: 1},
 }
 
 var injuryWeightsInjuryPronePlayers = []weightedrand.Choice{
-	{Item: false, Weight: 10},
+	{Item: false, Weight: 7},
 	{Item: true, Weight: 1},
 }
 
